b3f41b3810a86800cbccdc888490061c7a57e215
MyoThet <mr.myothet@gmail.com>
2026-04-06 20:22:21 +0700

    Reference Domain from Console App

--- TB5.NLayer.ConsoleApp/Program.cs
+++ TB5.NLayer.ConsoleApp/Program.cs
@@ -1,1 +1,16 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using TB5.Layer.Domain.Features.Product;
+using TB5.Layer.Domain.Features.Product.Models;
+
+Console.WriteLine("Hello, World!");
+
+ProductService productService = new ProductService();
+Console.WriteLine("\n Get Products");
+var products = productService.GetProducts();
+Console.WriteLine($"Total Products: {products.Count}");
+
+// Create Product
+var createRequest = new ProductCreateRequest { Name = "Console Test Product", Price = 99.99m };
+var createResponse = productService.CreateProduct(createRequest);
+Console.WriteLine($"Create Product Success: {createResponse.IsSuccess}, Id: {createResponse.Id}");
+
+Console.ReadLine();
