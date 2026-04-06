08c7588432aeaf3a2ec3853192b7d00bee60a781
MyoThet <mr.myothet@gmail.com>
2026-04-06 20:09:27 +0700

    Add project files.

---
+++ TB5.Layer.Domain/Features/Product/Models/ProductCreateResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TB5.Layer.Domain.Features.Product.Models;
+
+public class ProductCreateResponse
+{
+    public bool IsSuccess { get; set; }
+    public string Message { get; set; }
+    public int Id { get; set; }
+}
---
+++ TB5.Layer.Domain/Features/Product/Models/ProductUpdateRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TB5.Layer.Domain.Features.Product.Models;
+
+public class ProductUpdateRequest
+{
+    public string Name { get; set; } = null!;
+    public decimal Price { get; set; }
+}
